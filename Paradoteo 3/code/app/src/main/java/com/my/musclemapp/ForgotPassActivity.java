0cf81ebf5292358f73a629c9311b3bdf5da98012
Nick Potamianos <up1084537@ac.upatras.gr>
2024-05-24 17:43:46 +0300

    init

---
+++ Paradoteo 3/code/app/src/main/java/com/my/musclemapp/ForgotPassActivity.java
@@ -0,0 +1,127 @@
+package com.my.musclemapp;
+
+import android.animation.*;
+import android.app.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import com.google.firebase.FirebaseApp;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+import org.json.*;
+
+public class ForgotPassActivity extends AppCompatActivity {
+	
+	private LinearLayout linear1;
+	private LinearLayout linear3;
+	private LinearLayout mob_text;
+	private LinearLayout linear5;
+	private LinearLayout btn;
+	private TextView textview1;
+	private TextView textview2;
+	private ImageView imageview1;
+	private EditText edittext2;
+	private TextView textview7;
+	private TextView textview3;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.forgot_pass);
+		initialize(_savedInstanceState);
+		FirebaseApp.initializeApp(this);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		linear1 = findViewById(R.id.linear1);
+		linear3 = findViewById(R.id.linear3);
+		mob_text = findViewById(R.id.mob_text);
+		linear5 = findViewById(R.id.linear5);
+		btn = findViewById(R.id.btn);
+		textview1 = findViewById(R.id.textview1);
+		textview2 = findViewById(R.id.textview2);
+		imageview1 = findViewById(R.id.imageview1);
+		edittext2 = findViewById(R.id.edittext2);
+		textview7 = findViewById(R.id.textview7);
+		textview3 = findViewById(R.id.textview3);
+	}
+	
+	private void initializeLogic() {
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
---
+++ Paradoteo 3/code/app/src/main/java/com/my/musclemapp/LoginActivity.java
@@ -0,0 +1,289 @@
+package com.my.musclemapp;
+
+import android.animation.*;
+import android.app.*;
+import android.app.Activity;
+import android.content.*;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.net.Uri;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.*;
+import org.json.*;
+
+public class LoginActivity extends AppCompatActivity {
+	
+	private FirebaseDatabase _firebase = FirebaseDatabase.getInstance();
+	
+	private ArrayList<HashMap<String, Object>> users_listmap = new ArrayList<>();
+	
+	private LinearLayout linear1;
+	private LinearLayout linear3;
+	private LinearLayout mob_text;
+	private LinearLayout linear4;
+	private LinearLayout linear5;
+	private LinearLayout btn;
+	private LinearLayout linear6;
+	private LinearLayout linear7;
+	private LinearLayout linear11;
+	private LinearLayout linear12;
+	private LinearLayout linear13;
+	private TextView textview1;
+	private TextView textview2;
+	private ImageView imageview1;
+	private EditText edittext2;
+	private ImageView imageview3;
+	private EditText edittext3;
+	private ImageView imageview4;
+	private TextView textview7;
+	private TextView textview3;
+	private LinearLayout lineleft;
+	private TextView textview4;
+	private LinearLayout lineright;
+	private TextView textview8;
+	private TextView textview10;
+	private TextView textview11;
+	private TextView textview12;
+	private TextView textview13;
+	private TextView textview14;
+	private TextView textview15;
+	private TextView textview16;
+	private TextView textview17;
+	
+	private DatabaseReference Users = _firebase.getReference("Users");
+	private ChildEventListener _Users_child_listener;
+	private SharedPreferences users_data;
+	private SharedPreferences userid;
+	private SharedPreferences first_run;
+	private Intent open = new Intent();
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.login);
+		initialize(_savedInstanceState);
+		FirebaseApp.initializeApp(this);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		linear1 = findViewById(R.id.linear1);
+		linear3 = findViewById(R.id.linear3);
+		mob_text = findViewById(R.id.mob_text);
+		linear4 = findViewById(R.id.linear4);
+		linear5 = findViewById(R.id.linear5);
+		btn = findViewById(R.id.btn);
+		linear6 = findViewById(R.id.linear6);
+		linear7 = findViewById(R.id.linear7);
+		linear11 = findViewById(R.id.linear11);
+		linear12 = findViewById(R.id.linear12);
+		linear13 = findViewById(R.id.linear13);
+		textview1 = findViewById(R.id.textview1);
+		textview2 = findViewById(R.id.textview2);
+		imageview1 = findViewById(R.id.imageview1);
+		edittext2 = findViewById(R.id.edittext2);
+		imageview3 = findViewById(R.id.imageview3);
+		edittext3 = findViewById(R.id.edittext3);
+		imageview4 = findViewById(R.id.imageview4);
+		textview7 = findViewById(R.id.textview7);
+		textview3 = findViewById(R.id.textview3);
+		lineleft = findViewById(R.id.lineleft);
+		textview4 = findViewById(R.id.textview4);
+		lineright = findViewById(R.id.lineright);
+		textview8 = findViewById(R.id.textview8);
+		textview10 = findViewById(R.id.textview10);
+		textview11 = findViewById(R.id.textview11);
+		textview12 = findViewById(R.id.textview12);
+		textview13 = findViewById(R.id.textview13);
+		textview14 = findViewById(R.id.textview14);
+		textview15 = findViewById(R.id.textview15);
+		textview16 = findViewById(R.id.textview16);
+		textview17 = findViewById(R.id.textview17);
+		users_data = getSharedPreferences("users_data", Activity.MODE_PRIVATE);
+		userid = getSharedPreferences("userid", Activity.MODE_PRIVATE);
+		first_run = getSharedPreferences("first_run", Activity.MODE_PRIVATE);
+		
+		btn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				if (!"".equals(edittext2.getText().toString())) {
+					if (new Gson().toJson(users_listmap).contains(edittext2.getText().toString())) {
+						SketchwareUtil.showMessage(getApplicationContext(), "Successful login");
+						userid.edit().putString("userid", edittext2.getText().toString()).commit();
+						first_run.edit().putString("first_run", "no").commit();
+						open.setClass(getApplicationContext(), MainActivity.class);
+						startActivity(open);
+						finish();
+					}
+					else {
+						open.setClass(getApplicationContext(), RegisterActivity.class);
+						startActivity(open);
+						finish();
+					}
+				}
+				else {
+					SketchwareUtil.showMessage(getApplicationContext(), "Please enter your mobile number!");
+				}
+			}
+		});
+		
+		_Users_child_listener = new ChildEventListener() {
+			@Override
+			public void onChildAdded(DataSnapshot _param1, String _param2) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				Users.addListenerForSingleValueEvent(new ValueEventListener() {
+					@Override
+					public void onDataChange(DataSnapshot _dataSnapshot) {
+						users_listmap = new ArrayList<>();
+						try {
+							GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+							for (DataSnapshot _data : _dataSnapshot.getChildren()) {
+								HashMap<String, Object> _map = _data.getValue(_ind);
+								users_listmap.add(_map);
+							}
+						}
+						catch (Exception _e) {
+							_e.printStackTrace();
+						}
+						if (!users_data.getString(_childKey, "").contains("UID")) {
+							users_data.edit().putString(_childKey, "UID").commit();
+							SketchwareUtil.showMessage(getApplicationContext(), "Created");
+						}
+						else {
+							
+						}
+					}
+					@Override
+					public void onCancelled(DatabaseError _databaseError) {
+					}
+				});
+			}
+			
+			@Override
+			public void onChildChanged(DataSnapshot _param1, String _param2) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				
+			}
+			
+			@Override
+			public void onChildMoved(DataSnapshot _param1, String _param2) {
+				
+			}
+			
+			@Override
+			public void onChildRemoved(DataSnapshot _param1) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				
+			}
+			
+			@Override
+			public void onCancelled(DatabaseError _param1) {
+				final int _errorCode = _param1.getCode();
+				final String _errorMessage = _param1.getMessage();
+				
+			}
+		};
+		Users.addChildEventListener(_Users_child_listener);
+	}
+	
+	private void initializeLogic() {
+		mob_text.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)16, 0xFF121212));
+		linear4.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)16, 0xFF121212));
+		btn.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)12, 0xFFE53535));
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
