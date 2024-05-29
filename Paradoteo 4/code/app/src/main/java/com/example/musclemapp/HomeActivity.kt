68fd3d29eb2918d1bbf4c9b14eba48dda92775e6
Nick Potamianos <up1084537@ac.upatras.gr>
2024-05-29 17:18:31 +0300

    some kotlin to run

---
+++ Paradoteo 4/code/app/src/main/java/com/example/musclemapp/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.musclemapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+    }
+}
--- Paradoteo 4/code/app/src/main/java/com/example/musclemapp/MainActivity.kt
+++ Paradoteo 4/code/app/src/main/java/com/example/musclemapp/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.musclemapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewPager: ViewPager
+    private lateinit var pagerAdapter: ScreenSlidePagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_main) // Ensure this line references the correct layout file
+
+        viewPager = findViewById(R.id.viewPager)
+        pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        viewPager.adapter = pagerAdapter
+    }
+
+    fun navigateToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun navigateToHome() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
