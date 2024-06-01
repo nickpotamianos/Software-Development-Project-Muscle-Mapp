2ef4b23a08242359d06a255a611200c96eb0239d
Mako-M <82819764+Mako-M@users.noreply.github.com>
2024-06-01 17:46:25 +0300

    commit

--- Paradoteo 4/code/Android Studio Project/app/src/main/java/com/example/musclemapp/SignUpScreen.kt
+++ Paradoteo 4/code/Android Studio Project/app/src/main/java/com/example/musclemapp/SignUpScreen.kt
@@ -7,19 +7,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginScreen : AppCompatActivity() {
+class SignUpScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.login)
+        setContentView(R.layout.signup)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val pressForLogin = findViewById<androidx.cardview.widget.CardView>(R.id.LogInButton)
-        pressForLogin.setOnClickListener{
+        val pressForSignUp = findViewById<androidx.cardview.widget.CardView>(R.id.SignInButton)
+        pressForSignUp.setOnClickListener{
             val intent = Intent(this, OpeningScreen2::class.java)
             startActivity(intent)
         }
