8dea0d5c424332dbce770f99246dc1458073c240
Mako-M <82819764+Mako-M@users.noreply.github.com>
2024-06-02 03:44:08 +0300

    Update

---
+++ Paradoteo 4/code/MuscleMapp/app/src/main/java/com/example/musclemapp/SavedSpacesScreen.kt
@@ -0,0 +1,20 @@
+package com.example.musclemapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SavedSpacesScreen : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.savedspaces)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- Paradoteo 4/code/MuscleMapp/app/src/main/java/com/example/musclemapp/SearchBarMenu.kt
+++ Paradoteo 4/code/MuscleMapp/app/src/main/java/com/example/musclemapp/SearchBarMenu.kt
@@ -3,16 +3,16 @@ package com.example.musclemapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeScreen : BaseActivity() {
+class SearchBarMenu : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.home)
+        setContentView(R.layout.search)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -28,18 +28,6 @@ class HomeScreen : BaseActivity() {
         val goToNotifications = findViewById<ImageView>(R.id.notbell)
         goToNotifications.setOnClickListener{
             val intent = Intent(this, NotificationScreen::class.java)
-            startActivity(intent)
-        }
-
-        val searchBar = findViewById<LinearLayout>(R.id.linear27)
-        searchBar.setOnClickListener{
-            val intent = Intent(this, SearchBarMenu::class.java)
-            startActivity(intent)
-        }
-
-        val goToMap = findViewById<ImageView>(R.id.imageview37)
-        goToMap.setOnClickListener{
-            val intent = Intent(this, MapScreen::class.java)
             startActivity(intent)
         }
 
